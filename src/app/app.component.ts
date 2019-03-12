b45a866c99832d3d740f7f26a8a88c6c1710fb4f
Agustin Haller <agustinhaller@gmail.com>
2019-03-12 13:21:29 -0300

    refactor: add explanation to the different demos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,63 +12,60 @@ export class AppComponent {
   public appPages = [
     {
       title: 'Home',
-      url: '/home',
-      icon: 'home'
-    },
+      url: '/home'
+    }
+  ];
+
+  public moduleLoadingPages = [
     {
       title: 'Eagerly Loaded',
-      url: '/eagerly-loaded',
-      icon: 'list'
+      url: '/eagerly-loaded'
     },
     {
       title: 'Lazy Loaded',
-      url: '/lazy-loaded',
-      icon: 'list'
+      url: '/lazy-loaded'
     },
     {
       title: 'Pre Loaded',
-      url: '/pre-loaded',
-      icon: 'list'
+      url: '/pre-loaded'
     },
     {
       title: 'Pre Loaded (with delay)',
-      url: '/pre-loaded-with-delay',
-      icon: 'list'
+      url: '/pre-loaded-with-delay'
     },
     {
       title: 'Listing',
-      url: '/listing',
-      icon: 'list'
-    },
+      url: '/listing'
+    }
+  ];
+
+  public routeGuardsPages = [
     {
       title: 'Can Activate',
-      url: '/can-activate',
-      icon: 'list'
+      url: '/can-activate'
     },
     {
       title: 'Can Deactivate',
-      url: '/can-deactivate',
-      icon: 'list'
+      url: '/can-deactivate'
     },
     {
       title: 'Can Load',
-      url: '/can-load',
-      icon: 'list'
-    },
+      url: '/can-load'
+    }
+  ];
+
+  public routeResolversPages = [
     {
       title: 'Blocking Resolver',
-      url: '/blocking-resolver',
-      icon: 'list'
+      url: '/blocking-resolver'
     },
     {
       title: 'Non Blocking Resolver',
-      url: '/non-blocking-resolver',
-      icon: 'list'
+      url: '/non-blocking-resolver'
     },
     {
       title: 'Progressive Shell Resolver',
-      url: '/progressive-shell-resolver',
-      icon: 'list'
+      url: '/progressive-shell-resolver'
     }
   ];
 
